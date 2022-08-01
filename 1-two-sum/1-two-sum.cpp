296c82116ba7afdac1c795aef6d85ecd6cb1031b
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-08-01 23:36:25 +0530

    Time: 996 ms (6.55%), Space: 10 MB (96.32%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         vector<int> ans;
+        
         for(int i=0;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
@@ -12,6 +13,6 @@ public:
                 }
             }
         }
-    return ans;
+        return ans;
     } 
 };
